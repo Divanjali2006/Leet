e25222dd0f585bc3c144d07c8238bf360193e8a9
Divanjali2006 <23H41A4549@bvcits.edu.in>
2026-05-27 05:39:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (14.00%)

---
+++ 1960-check-if-the-sentence-is-pangram/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        if len(set(sentence))==26:
+            return True
+        else:
+            return False
+
+        
