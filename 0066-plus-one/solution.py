700a6f962f5b15f270c2a8cd37a23f755e7a06ab
Divanjali2006 <23H41A4549@bvcits.edu.in>
2026-06-10 06:28:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (55.90%)

--- 0066-plus-one/solution.py
+++ 0066-plus-one/solution.py
@@ -1,8 +1,13 @@
+
 class Solution:
-    def plusOne(self, digits):
-        s=''.join(map(str,digits))
-        num=int(s)+1
-        result=list(map(int,str(num)))
-        return result
-    
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        
+        # If all digits were 9
+        return [1] + digits
         
